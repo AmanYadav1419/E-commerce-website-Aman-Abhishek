6f6e262722162b489b0cbbd3d3b246011c9ad5d0
unknown <amanyadav16544@gmail.com>
2024-03-11 11:24:46 +0530

    created Home and Blog component & gie them a route,path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-
+import { Outlet } from "react-router-dom";
 import './App.css'
 
 function App() {
-
-
   return (
     <>
-      <h1 className='text-primary m-5 p-5'>hello Devloper</h1>
+    <nav>Navbar</nav>
+     <Outlet/>
     </>
   )
 }
