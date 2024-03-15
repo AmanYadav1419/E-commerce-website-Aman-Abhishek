07e79aeb4b808413c8f92be27a004085475db406
unknown <amanyadav16544@gmail.com>
2024-03-15 15:53:32 +0530

    ProductShowCase Section or CategoryShowCase is done

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Banner from './Banner'
 import HomeCategory from './HomeCategory'
+import CategoryShowCase from './CategoryShowCase'
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <HomeCategory />
+      <CategoryShowCase />
     </div>
   )
 }
 
-export default Home
+export default Home;
