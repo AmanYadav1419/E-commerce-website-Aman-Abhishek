902ffdc906dd3f94ee9fdd4db51d60a054345aa7
unknown <amanyadav16544@gmail.com>
2024-03-16 10:57:12 +0530

    added LocationSpread.jsx || Happy customers

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from './Banner'
 import HomeCategory from './HomeCategory'
 import CategoryShowCase from './CategoryShowCase'
 import Register from './Register'
+import LocationSpread from './LocationSpread'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <HomeCategory />
       <CategoryShowCase />
       <Register />
+      <LocationSpread />
     </div>
   )
 }
