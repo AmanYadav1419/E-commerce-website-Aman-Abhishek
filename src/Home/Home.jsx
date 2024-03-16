e304887f2b091be157921624a1107696c3d2d6fa
unknown <amanyadav16544@gmail.com>
2024-03-16 16:16:01 +0530

    added Sponsor.jsx || Sponsor Section

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -6,6 +6,7 @@ import Register from './Register'
 import LocationSpread from './LocationSpread'
 import AboutUs from './AboutUs'
 import AppSection from './AppSection'
+import Sponsor from './Sponsor'
 
 const Home = () => {
   return (
@@ -17,6 +18,7 @@ const Home = () => {
       <LocationSpread />
       <AboutUs />
       <AppSection />
+      <Sponsor />
     </div>
   )
 }
