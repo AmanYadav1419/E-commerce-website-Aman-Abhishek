5f405e229d4e5707bb4ef95cbadf32f768249430
unknown <amanyadav16544@gmail.com>
2024-03-17 20:59:06 +0530

    added Pagination.jsx || Pagination Function and added styling to App.css

--- src/shop/Shop.jsx
+++ src/shop/Shop.jsx
@@ -1,51 +1,74 @@
-import React, { useState } from 'react'
-import PageHeader from '../components/PageHeader'
+import React, { useState } from "react";
+import PageHeader from "../components/PageHeader";
 
-const showResults = "Showing 01 - 12 of 139 Results"
+const showResults = "Showing 01 - 12 of 139 Results";
 import Data from "../products.json";
-import ProductCards from './ProductCards';
+import ProductCards from "./ProductCards";
+import Pagination from "./Pagination";
 
 const Shop = () => {
-    const [GridList, setGridList] = useState(true);
-    const [products, setProducts] = useState(Data);
-    console.log(products);
+  const [GridList, setGridList] = useState(true);
+  const [products, setProducts] = useState(Data);
+//   console.log(products);
+
+  // pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const productsPerPage = 12;
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage; 
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+//   function to change the curent page
+const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber)
+}
 
   return (
     <div>
-        <PageHeader title="Our Shop Page" curPage="Shop"/>
-        {/* shop page */}
-        <div className="shop-page padding-tb">
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-lg-8 col-12">
-                        <article>
-                            {/* layout and title here */}
-                            <div className="shop-title d-flex flex-warp justify-content-between">
-                                <p>{showResults}</p>
-                                <div className={`product-view-mode ${GridList ? "gridActive" : "listActive"}`}>
-                                    <a className='grid' onClick={() => setGridList(!GridList)}>
-                                        <i className="icofont-ghost"></i>
-                                    </a>
-                                    <a className='list' onClick={() => setGridList(!GridList)}>
-                                        <i className='icofont-listine-dots'></i>
-                                    </a>
-                                </div>
-                            </div>
-
-                            {/* product cards*/}
-                            <div>
-                                <ProductCards GridList={GridList} products={products}/>
-                            </div>
-                        </article>
-                    </div>
-                    <div className="col-lg-4 col-12">
-                        right side
-                    </div>
+      <PageHeader title="Our Shop Page" curPage="Shop" />
+      {/* shop page */}
+      <div className="shop-page padding-tb">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-12">
+              <article>
+                {/* layout and title here */}
+                <div className="shop-title d-flex flex-warp justify-content-between">
+                  <p>{showResults}</p>
+                  <div
+                    className={`product-view-mode ${
+                      GridList ? "gridActive" : "listActive"
+                    }`}
+                  >
+                    <a className="grid" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-ghost"></i>
+                    </a>
+                    <a className="list" onClick={() => setGridList(!GridList)}>
+                      <i className="icofont-listine-dots"></i>
+                    </a>
+                  </div>
                 </div>
-            </div>
-        </div>
-    </div>
-  )
-}
 
-export default Shop
+                {/* product cards*/}
+                <div>
+                  <ProductCards GridList={GridList} products={currentProducts} />
+                </div>
+
+                <Pagination 
+                productsPerPage={productsPerPage}
+                totalProducts = {products.length}
+                paginate={paginate}
+                activePage={currentPage}
+                />
+              </article>
+            </div>
+            <div className="col-lg-4 col-12">right side</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Shop;
