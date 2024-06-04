1281ba26337988a97c1a6635f72d04431e719956
unknown <amanyadav16544@gmail.com>
2024-06-04 15:22:55 +0530

    done upto details - Product name

--- src/shop/SingleProduct.jsx
+++ src/shop/SingleProduct.jsx
@@ -3,10 +3,11 @@ import { useParams } from "react-router-dom";
 import PageHeader from "../components/PageHeader";
 
 // Import swiper react components
-import { Swiper,SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import "swiper/css"
+import "swiper/css";
+import { Autoplay } from "swiper/modules";
 
 const SingleProduct = () => {
   const [Products, setProducts] = useState([]);
@@ -28,36 +29,74 @@ const SingleProduct = () => {
     <div>
       <PageHeader
         title={"Our Shop Single Page"}
-        curPage={"Shop/Single Product"}
+        curPage={"Shop/ Single Product"}
       />
       <div className="shop-single padding-tb aside-bg">
         <div className="container">
-            <div className="row justify-content-center">
-                {/* Left side */}
-                <div className="col-lg-8 col-12">
-                    <article>
-                        <div className="product-details">
-                            <div className="row align-items-center">
-                                {/* Image */}
-                                <div className="col-md-6 col-12">
-                                    <div className="product-thumb">
-                                        <div className="swiper-container pro-single-top"></div>
-                                    </div>
+          <div className="row justify-content-center">
+            {/* Left side */}
+            <div className="col-lg-8 col-12">
+              <article>
+                <div className="product-details">
+                  <div className="row align-items-center">
+                    {/* Image */}
+                    <div className="col-md-6 col-12">
+                      <div className="product-thumb">
+                        <div className="swiper-container pro-single-top">
+                          <Swiper
+                            spaceBetween={30}
+                            slidesPerView={1}
+                            Loop={"true"}
+                            autoplay={{
+                              delay: 2000,
+                              disableOnInteraction: false,
+                            }}
+                            modules={[Autoplay]}
+                            navigation={{
+                              prevEl: ".pro-single-prev",
+                              nextEl: ".pro-single-next",
+                            }}
+                            className="mySwiper"
+                          >
+                            {result.map((item, index) => {
+                              <SwiperSlide key={index}>
+                                <div className="single-thumb">
+                                  <img src={item.img} alt="" />
                                 </div>
-                                <div className="col-md-6 col-12">Details</div>
-                            </div>
+                              </SwiperSlide>;
+                            })}
+                          </Swiper>
+                          <div className="pro-single-next">
+                            <i className="ico-rounded-left"></i>
+                          </div>
+                          <div className="pro-single-prev">
+                            <i className="ico-rounded-right"></i>
+                          </div>
                         </div>
+                      </div>
+                    </div>
 
-                        {/* reviews */}
-                        <div className="review">
-                            Review
+                    {/* Details section */}
+                    <div className="col-md-6 col-12">
+                      <div className="post-content">
+                        <div>
+                          {result.map((item) => {
+                            <p key={item.id}>{item.name}</p>;
+                          })}
                         </div>
-                    </article>
+                      </div>
+                    </div>
+                  </div>
                 </div>
 
-                {/* Right side */}
-                <div className="col-lg-4 col-12">Right Side</div>
+                {/* reviews */}
+                <div className="review">Review</div>
+              </article>
             </div>
+
+            {/* Right side */}
+            <div className="col-lg-4 col-12">Right Side</div>
+          </div>
         </div>
       </div>
     </div>
