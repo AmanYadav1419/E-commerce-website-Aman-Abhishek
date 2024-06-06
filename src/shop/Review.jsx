a4d76c676d3839cc3beded54c9aa0bbcce167c6a
unknown <amanyadav16544@gmail.com>
2024-06-06 15:14:47 +0530

    review section is completed description section is remaining in Review.jsx

--- src/shop/Review.jsx
+++ src/shop/Review.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Ratting from "../components/Ratting";
 const reviwtitle = "Add a Review";
 
 let ReviewList = [
@@ -37,10 +38,99 @@ const Review = () => {
 
   return (
     <>
-      <ul className={`review-nav lab-ul ${reviewShow ? "RevActive" : "DescActive"}`}>
-        <li className="desc" onClick={() => setReviewShow(!reviewShow)}>Description</li>
-        <li className="rev" onClick={() => setReviewShow(!reviewShow)}>Review 4</li>
+      <ul
+        className={`review-nav lab-ul ${
+          reviewShow ? "RevActive" : "DescActive"
+        }`}
+      >
+        <li className="desc" onClick={() => setReviewShow(!reviewShow)}>
+          Description
+        </li>
+        <li className="rev" onClick={() => setReviewShow(!reviewShow)}>
+          Review 4
+        </li>
       </ul>
+
+      {/* desc and review content */}
+      <div
+        className={`review-content ${
+          reviewShow ? "review-content-show" : "description-content-show"
+        }`}
+      >
+        <div className="review-showing">
+          <ul className="content lab-ul">
+            {ReviewList.map((review, index) => (
+              <li key={index}>
+                <div className="post-thumb">
+                  <img src={review.imgUrl} alt={review.imgAlt} />
+                </div>
+                <div className="post-content">
+                  <div className="entry-meta">
+                    <div className="posted-on">
+                      <a href="#">{review.name}</a>
+                      <p>{review.date}</p>
+                    </div>
+                  </div>
+                  <div className="entry-content">
+                    <p>{review.desc}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+
+          {/* add review feild */}
+          <div className="client-review">
+            <div className="review-form">
+              <div className="review-title">
+                <h5>{reviwtitle}</h5>
+              </div>
+
+              <form action="action" className="row">
+                <div className="col-md-4 col-12">
+                  <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Full name *"
+                  />
+                </div>
+                <div className="col-md-4 col-12">
+                  <input
+                    type="text"
+                    name="email"
+                    id="email"
+                    placeholder="Your Email *"
+                  />
+                </div>
+                <div className="col-md-4 col-12">
+                  <div className="rating">
+                    <span className="me-2">Your Rating </span>
+                    <Ratting />
+                  </div>
+                </div>
+
+                <div className="col-md-12 col-12">
+                  <textarea
+                    name="message"
+                    id="message"
+                    rows="8"
+                    placeholder="Type Your Message Here"
+                  ></textarea>
+                </div>
+
+                <div className="col-12">
+                  <button type="submit" className="default-button">
+                    <span>submit Review</span>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+
+        {/* description */}
+      </div>
     </>
   );
 };
