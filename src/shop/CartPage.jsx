4afeb727374366b9e1f5533224b70fc6f2d97658
unknown <amanyadav16544@gmail.com>
2024-06-07 14:43:29 +0530

    UI part is done upto quantity part

--- src/shop/CartPage.jsx
+++ src/shop/CartPage.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import PageHeader from "../components/PageHeader";
+import { Link } from "react-router-dom";
 
 const CartPage = () => {
   const [cartItems, setCartItems] = useState([]);
@@ -63,7 +65,64 @@ const CartPage = () => {
   // order total
   const orderTotal = cartSubTotal;
 
-  return <div>CartPage</div>;
+  return (
+    <div>
+      <PageHeader title={"Shop Cart"} curPage={"Cart Page"} />
+
+      <div className="shop-cart padding-tb">
+        <div className="container">
+          <div className="section-wrapper">
+            {/* cart top */}
+            <div className="cart-top">
+              <table>
+                <thead>
+                  <tr>
+                    <th className="cat-product">Product</th>
+                    <th className="cat-price">Price</th>
+                    <th className="cat-quantity">Quantity</th>
+                    <th className="cat-toprice">Total</th>
+                    <th className="cat-edit">Edit</th>
+                  </tr>
+                </thead>
+
+                {/* table body */}
+                <tbody>
+                  {cartItems.map((item, index) => (
+                    <tr key={index}>
+                      <td className="product-item cat-product">
+                        <div className="p-thumb">
+                          <Link to="/shop">
+                            <img src={item.img} alt="" />
+                          </Link>
+                        </div>
+
+                        <div className="p-content">
+                          <Link to="/shop">{item.name}</Link>
+                        </div>
+                      </td>
+
+                        <td className="cat-price">$ {item.price}</td>
+                        <td className="cat-quantity">
+                            <div className="cart-plus-minus">
+                                <div className="dec qtybutton" onClick={()=> handleDecreaseQuantity(item)}>-</div>
+                                <input type="text" className="cart-plus-minus-box" name="qtybutton" value={item.quantity} />
+                                <div className="inc qtybutton" onClick={()=> handleIncreaseQuantity(item)}>+</div>
+
+                            </div>
+                        </td>
+
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            {/*-------------- cart top ends --------------*/}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
